d6f9b754d5f8d03ace14f1ba8ec71d04b4aa03ef
kotet912 <kotet912@yandex.ru>
2024-10-25 22:58:34 +0300

    Состояние теста

--- stores/quiz.js
+++ stores/quiz.js
@@ -1,1 +1,12 @@
 // Состояние теста
+import { defineStore } from 'pinia';
+
+export const useQuizStore = defineStore('quiz', {
+  state: () => ({
+    questions: [],
+    currentQuestionIndex: 0,
+    userAnswers: [],
+  }),
+  actions: {
+  },
+});
