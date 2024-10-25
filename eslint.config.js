bb796a36a0279dde70d25e0e54ad5a670d0be9f5
kotet912 <kotet912@yandex.ru>
2024-10-26 00:22:53 +0300

    Инициализация вопросов Перемешивание вопросов Перемешивание ответов для каждого вопроса

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,11 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginVue from 'eslint-plugin-vue';
-import pluginPrettier from 'eslint-plugin-prettier';
-import configPrettier from 'eslint-config-prettier';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+
 
 export default [
-  {
-    files: ['**/*.{js,mjs,cjs,vue}'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        defineProps: 'readonly',
-        defineEmits: 'readonly',
-        defineExpose: 'readonly',
-        withDefaults: 'readonly',
-      },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      vue: pluginVue,
-      prettier: pluginPrettier,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  },
+  {files: ["**/*.{js,mjs,cjs,vue}"]},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  pluginVue.configs['recommended'],
-  configPrettier,
+  ...pluginVue.configs["flat/essential"],
 ];
--- stores/quiz.js
+++ stores/quiz.js
@@ -1,5 +1,7 @@
 // Состояние теста
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
+import questionsData from '@/data/questions';
+
 
 export const useQuizStore = defineStore('quiz', {
   state: () => ({
@@ -8,5 +10,40 @@ export const useQuizStore = defineStore('quiz', {
     userAnswers: [],
   }),
   actions: {
+    // Инициализация вопросов
+    initQuestions () {
+      this.questions = questionsData.map((item) => {
+        const correctAnswerIndex = item.answers.findIndex((answer) =>
+          answer.endsWith('(+)')
+        );
+        return {
+          question: item.question,
+          answers: item.answers.map((answer) =>
+            answer.replace(' (+)', '').trim()
+          ),
+          correctAnswerIndex,
+        };
+      });
+      // Перемешивание вопросов
+      this.questions = this.questions.sort(() => Math.random() - 0.5);
+
+      // Перемешивание ответов для каждого вопроса
+      this.questions = this.questions.map((item) => {
+        const answers = item.answers.map((answer, index) => ({
+          text: answer,
+          index,
+        }));
+
+        const shuffledAnswers = answers.sort(() => Math.random() - 0.5);
+
+        return {
+          ...item,
+          answers: shuffledAnswers,
+          correctAnswerIndex: shuffledAnswers.findIndex(
+            (answer) => answer.index === item.correctAnswerIndex
+          ),
+        };
+      });
+    },
   },
 });
